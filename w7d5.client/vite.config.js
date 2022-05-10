18d238233a057191708ade12127bfaf46d36d266
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-05-09 21:37:29 -0600

    heroku commit

--- w7d5.client/vite.config.js
+++ w7d5.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../w7d5/client',
     sourcemap: false
   },
   server: {
